911f5eee69b1f2f9bb9023b1518fc8daad5257ba
NitzanSelwyn <nitzansel@gmail.com>
2024-08-30 17:54:41 +0300

    adding bullets and collition

--- player.py
+++ player.py
@@ -1,11 +1,13 @@
 from circleshape import CircleShape
 import pygame
 import constants
+from shot import Shot
 
 class Player(CircleShape):
     def __init__(self, x ,y):
         super().__init__(x,y,constants.PLAYER_RADIUS)
         self.rotation = 0
+        self.shoot_timer = 0
 
     # in the player class
     def triangle(self):
@@ -21,6 +23,7 @@ class Player(CircleShape):
 
     def update(self, dt):
         keys = pygame.key.get_pressed()
+        self.shoot_timer -= dt
 
         if keys[pygame.K_a]:
             self.rotate(-dt)
@@ -31,6 +34,15 @@ class Player(CircleShape):
             self.move(dt)
         if keys[pygame.K_s]:
             self.move(-dt)
+        if keys[pygame.K_SPACE]:
+            self.shoot()
+
+    def shoot(self):
+        if self.shoot_timer > 0:
+            return
+        self.shoot_timer = constants.PLAYER_SHOOT_COOLDOWN
+        shot = Shot(self.position.x, self.position.y)
+        shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * constants.PLAYER_SHOOT_SPEED
 
     def rotate(self,dt):
         self.rotation += constants.PLAYER_TURN_SPEED * dt
