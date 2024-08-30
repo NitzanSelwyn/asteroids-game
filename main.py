20be818a002c21889752e2a9499ead0be96a06b4
NitzanSelwyn <nitzansel@gmail.com>
2024-08-30 16:22:47 +0300

    finsih player movement

--- main.py
+++ main.py
@@ -1,20 +1,40 @@
 import pygame
-import constants as const
+from constants import *
+from player import Player
+
 
 def main():
-    #print("Starting asteroids!")
-    #print(f'Screen width: {const.SCREEN_WIDTH}')
-    #print(f'Screen height: {const.SCREEN_HEIGHT}')
     pygame.init()
-    screen = pygame.display.set_mode((const.SCREEN_WIDTH, const.SCREEN_HEIGHT))
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    dt = 0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-            
-        screen.fill("black", rect=None, special_flags=0)
+
+        for obj in updatable:
+            obj.update(dt)
+
+        screen.fill("black")
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()
+
+        # limit the framerate to 60 FPS
+        dt = clock.tick(60) / 1000
+
 
 if __name__ == "__main__":
     main()
